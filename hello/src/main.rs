dc2e7d4ea1c088bf61e369410cedc625c7dca664
cpucorecore <cpucorecore@gmail.com>
2022-02-28 20:31:18 +0800

    parse params from arguments

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
 	assert!(n!=0 && m!=0);
 	while m != 0 {
@@ -13,6 +16,21 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 
 fn main() {
     println!("Hello, world!");
+	let mut numbers = Vec::new();
+	for arg in std::env::args().skip(1) {
+		numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+	}
+
+	if numbers.len() == 0 {
+		writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+	}
+
+	let mut d = numbers[0];
+	for m in &numbers[1..] {
+		d = gcd(d, *m);
+	}
+
+	println!("The greasted common divisor of {:?} is {}", numbers, d);
 }
 
 #[test]
