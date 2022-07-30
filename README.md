3c2036027979f31f4fc45f3fd8e080c00538cf2e
cpucorecore <cpucorecore@gmail.com>
2022-07-31 01:32:41 +0800

    add demo for posting a file to api

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ cargo run
 cargo clean
 cargo test
 ```
+
+## how to post a file to api?
+[reqwest](https://docs.rs/reqwest/0.11.11/reqwest/blocking/struct.RequestBuilder.html#method.multipart)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 mod iterator;
 
+use reqwest::blocking::multipart;
+use reqwest::blocking::Client;
+
 fn main() {
-    iterator::gen_random_files(1024*1024*2, 1000);
-    println!("Hello, world!");
+    iterator::gen_random_files(1024*1024*100, 1);
+    let part = multipart::Form::new().file("file", "/home/sky/data/test/1").unwrap();
+    let client = Client::new();
+    let res = client
+        .post("http://127.0.0.1:5001/api/v0/add")
+        .multipart(part)
+        .send().unwrap();
+
+    println!("{:?}", res);
 }
